a3a09fe794bdcabd0ebb552b459441754511db67
owenreal <162478543+owenreal@users.noreply.github.com>
2024-10-31 14:35:59 -0500

    10/31 states start and failing at tilemap anims

--- game-design-1/td_player.gd
+++ game-design-1/td_player.gd
@@ -1,7 +1,17 @@
 extends CharacterBody2D
 
-
 const SPEED = 100.0
+const MAXIMUM_OBTAINABLE_HEALTH = 400.0
+enum STATES { IDLE=0, DEAD, DAMAGED, ATTACKING, CHARGING }
+
+@export var data = {
+	"max_health": 60.0,
+	"health": 60.0,
+	"money": 0,
+	"state": STATES.IDLE,
+	"secondaries": [],
+}
+
 var inertia = Vector2()
 var look_direction = Vector2.DOWN
 
@@ -18,6 +28,7 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
 	velocity += inertia
+	update_animation(direction)
 	move_and_slide()
 	inertia = inertia.move_toward(Vector2.ZERO, delta*1000.0)
 	
@@ -25,4 +36,18 @@ func _physics_process(delta: float) -> void:
 		$Camera2D/pause_menu.show()
 		get_tree().paused = true
 		
+func update_animation(direction):
+	var a_name = "idle_"
+	if direction.length() > 0:
+		a_name = "walk_"
 		
+	if look_direction.x != 0:
+		a_name += "side" 
+		$AnimatedSprite2D.flip_h = look_direction.x < 0
+	elif look_direction.y < 0:
+		a_name += "up"
+	elif look_direction.y > 0:
+		a_name += "down"
+	$AnimatedSprite2D.animation = a_name
+	$AnimatedSprite2D.play()
+	pass
