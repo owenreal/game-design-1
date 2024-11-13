e87afbd0531eb60b77a7914db146f9de19bde740
owenreal <162478543+owenreal@users.noreply.github.com>
2024-11-13 14:35:42 -0600

    11/13 drops and shader

--- game-design-1/td_enemy.gd
+++ game-design-1/td_enemy.gd
@@ -51,6 +51,32 @@ signal recovered
 @onready var rcL = $RayCast2DL
 @onready var anim_player = $AnimatedSprite2D
 
+var drops = ["drop_coin", "drop_heart"]
+var coin_scene = preload("res://entities/items/mini_coin.tscn")
+var heart_scene = preload("res://entities/items/mini_heart.tscn")
+var damage_shader = preload("res://Assets/shaders/take_damage.tres")
+
+func vec2_offset():
+	return Vector2(randf_range(-10.0, 10.0), randf_range(-10.0, 10.0))
+
+func drop_scene(item_scene):
+	item_scene.global_position = self.global_position + vec2_offset()
+	get_tree().current_scene.add_child(item_scene)
+
+func drop_heart():
+	drop_scene(heart_scene.instantiate())
+
+func drop_coin():
+	var coin = coin_scene.instantiate()
+	coin.value = self.money_value
+	drop_scene(coin)
+
+func drop_items():
+	var num_drops = randi() % 3 + 1 # 3 inclusive
+	for i in range(num_drops):
+		var rnd_drop = drops[randi() % drops.size()]
+		call_deferred(rnd_drop)
+
 func turn_toward_player_location(location: Vector2):
 	# set state to move toward player
 	var dir_to_player = (location - self.global_position).normalized()
@@ -73,9 +99,11 @@ func take_damage(dmg, attacker=null):
 		HEALTH -= dmg
 		damage_lock = 0.2
 		animation_lock = 0.2
-		# damage shader here
+		var dmg_intensity = clamp(1.0-((HEALTH+0.01)/MAX_HEALTH), 0.1, 0.8)
+		$AnimatedSprite2D.material = damage_shader.duplicate()
+		$AnimatedSprite2D.material.set_shader_parameter("intensity", dmg_intensity)
 		if HEALTH <= 0:
-			# drop item
+			drop_items()
 			# death sound
 			queue_free()
 		else:
@@ -96,7 +124,7 @@ func _physics_process(delta: float) -> void:
 			raydir.rotated(deg_to_rad(+45)).normalized() * vision_distance
 	if animation_lock == 0.0:
 		if AI_STATE == STATES.DAMAGED:
-			# reset shader
+			$AnimatedSprite2D.material = null
 			AI_STATE = STATES.IDLE
 			recovered.emit()
 		for player in get_tree().get_nodes_in_group("Player"):
